2c7fd9b54c3f4d87d72773cdec98e7964512f60a
mvarrone <mativarrone2@gmail.com>
2024-09-19 00:00:58 -0300

    revert to previous version (which it had not Enum implemented)

--- git_push.py
+++ git_push.py
@@ -2,7 +2,6 @@ import sys
 import subprocess
 import logging
 from datetime import datetime
-from enum import Enum, auto
 
 # Configure logging
 log_filename = "logs.txt"
@@ -10,8 +9,11 @@ logging.basicConfig(filename=log_filename,
                     level=logging.INFO, format="%(message)s")
 
 
-class AnsiColor(Enum):
-    """Enum for ANSI escape sequences for colors."""
+# Define constants for ANSI escape sequences for colors
+class AnsiColor:
+    """Constants for ANSI escape sequences for colors."""
+
+    # Color definitions
     RED = "\033[31m"
     GREEN = "\033[32m"
     YELLOW = "\033[33m"
@@ -21,55 +23,47 @@ class AnsiColor(Enum):
     RESET = "\033[0m"
 
 
-class MessageType(Enum):
-    """Enum for types of messages."""
-    SUCCESS = auto()
-    WARNING = auto()
-    FAILURE = auto()
-    INFORMATION = auto()
-    ERROR = auto()
+# Define constants for terminal outputs
+class Messages:
+    """Constants for terminal output messages."""
 
+    # Success
+    SUCCESS_FILES_STAGED = "FILES ADDED TO STAGING AREA: OK"
+    SUCCESS_COMMIT_CREATED = "COMMIT CREATED: OK"
+    SUCCESS_CHANGES_PUSHED = "CHANGES PUSHED: OK"
 
-class Messages(Enum):
-    """Enum for terminal output messages."""
-    SUCCESS_FILES_STAGED = (MessageType.SUCCESS,
-                            "FILES ADDED TO STAGING AREA: OK")
-    SUCCESS_COMMIT_CREATED = (MessageType.SUCCESS, "COMMIT CREATED: OK")
-    SUCCESS_CHANGES_PUSHED = (MessageType.SUCCESS, "CHANGES PUSHED: OK")
-    WARNING_DEFAULT_BRANCH_NAME_USED = (
-        MessageType.WARNING, "USING DEFAULT BRANCH NAME: ")
-    FAILURE_CHANGES_NOT_PUSHED = (
-        MessageType.FAILURE, "CHANGES HAVE NOT BEEN PUSHED")
-    FAILURE_KEYBOARD_INTERRUPT = (
-        MessageType.FAILURE, "YOU PRESSED CTRL+C TO END SCRIPT EXECUTION")
-    INFORMATION_TEXT = (MessageType.INFORMATION, "TRYING TO PUSH CHANGES...")
-    ERROR_TEXT = (MessageType.ERROR, "ERROR: ")
+    # Warnings
+    WARNING_DEFAULT_BRANCH_NAME_USED = "USING DEFAULT BRANCH NAME: "
+
+    # Failures
+    FAILURE_CHANGES_NOT_PUSHED = "CHANGES HAVE NOT BEEN PUSHED"
+    FAILURE_KEYBOARD_INTERRUPT = "YOU PRESSED CTRL+C TO END SCRIPT EXECUTION"
+
+    # Information
+    INFORMATION_TEXT = "TRYING TO PUSH CHANGES..."
+
+    # Errors
+    ERROR_TEXT = "ERROR: "
     ERROR_COMMIT_MESSAGE_EMPTY = (
-        MessageType.ERROR, "Commit message cannot be empty. Please, enter a valid commit message")
+        "Commit message cannot be empty. Please, enter a valid commit message"
+    )
     ERROR_GIT_NOT_INSTALLED = (
-        MessageType.ERROR, "Git is not installed or accessible. Please, install Git and try again.")
-
-    def __init__(self, message_type, text):
-        self.message_type = message_type
-        self.text = text
-
-
-def print_message(message: Messages, additional_text: str = ""):
-    """Print a formatted message based on its type."""
-    color = AnsiColor.RESET
-    if message.message_type == MessageType.SUCCESS:
-        color = AnsiColor.GREEN
-    elif message.message_type == MessageType.WARNING:
-        color = AnsiColor.YELLOW
-    elif message.message_type in (MessageType.FAILURE, MessageType.ERROR):
-        color = AnsiColor.RED
-
-    print(f"{color.value}{message.text}{
-          additional_text}{AnsiColor.RESET.value}")
+        "Git is not installed or accessible. Please, install Git and try again."
+    )
 
 
 def is_git_installed() -> bool:
-    """Check if Git is installed and accessible."""
+    """Check if Git is installed and accessible.
+
+    This function checks if Git is installed and accessible in the current environment
+    by attempting to execute the 'git --version' command. If the command executes
+    successfully, it returns True, indicating that Git is installed and accessible.
+    If the command fails (due to Git not being installed or not being accessible),
+    it returns False.
+
+    Returns:
+        bool: True if Git is installed and accessible, False otherwise.
+    """
     try:
         subprocess.check_output(["git", "--version"])
         return True
@@ -77,48 +71,85 @@ def is_git_installed() -> bool:
         return False
 
 
-def set_commit_message() -> str:
-    """Prompt the user to enter a commit message."""
+def get_commit_message() -> str:
+    """Prompt the user to enter a commit message.
+
+    Returns:
+        str: The commit message entered by the user.
+    """
     while True:
         commit_message = input("Please, enter some message for this commit: ")
         if commit_message:
             return commit_message
         else:
-            print_message(Messages.ERROR_COMMIT_MESSAGE_EMPTY)
+            print(
+                f"{AnsiColor.RED}{Messages.ERROR_COMMIT_MESSAGE_EMPTY}{
+                    AnsiColor.RESET}"
+            )
 
 
-def set_branch_name(default_branch_name: str) -> str:
-    """Prompt the user to enter a branch name, or use a default value."""
-    branch_name = input(f"Please, enter the branch name ({
-                        default_branch_name} if not specified): ")
+def get_branch_name(default_branch_name: str) -> str:
+    """Prompt the user to enter a branch name, or use a default value.
+
+    Args:
+        default_branch_name (str): The default branch name to use if none is specified.
+
+    Returns:
+        str: The branch name entered by the user, or the default branch name if none is specified.
+    """
+    branch_name = input(
+        f"Please, enter the branch name ({
+            default_branch_name} if not specified): "
+    )
     if branch_name:
         return branch_name
     else:
-        print_message(Messages.WARNING_DEFAULT_BRANCH_NAME_USED,
-                      f"'{default_branch_name}'")
+        print(
+            f"{AnsiColor.YELLOW}{Messages.WARNING_DEFAULT_BRANCH_NAME_USED}'{
+                default_branch_name}'{AnsiColor.RESET}"
+        )
         return default_branch_name
 
 
-def run_git_command(command: list[str], command_name: str, commit_message: str, branch_name: str) -> str:
-    """Run a git command and return the output."""
+def run_git_command(
+    command: list[str], command_name: str, commit_message: str, branch_name: str
+) -> str:
+    """Run a git command and return the output.
+
+    Args:
+        command (list[str]): The git command to execute as a list of strings.
+        command_name (str): The name of the git command for further error handling.
+
+    Returns:
+        str: The output of the executed git command.
+    """
     try:
         output = subprocess.check_output(
-            command, stderr=subprocess.STDOUT, universal_newlines=True)
+            command, stderr=subprocess.STDOUT, universal_newlines=True
+        )
         return output.strip()
     except subprocess.CalledProcessError as e:
         if "git add" in command_name:
-            print_message(Messages.ERROR_TEXT,
-                          "Error occurred during 'git add' operation.")
+            print(
+                f"{AnsiColor.RED}Error occurred during 'git add' operation.{
+                    AnsiColor.RESET}"
+            )
         elif "git commit" in command_name:
-            print_message(Messages.ERROR_TEXT,
-                          "Error occurred during 'git commit' operation.")
+            print(
+                f"{AnsiColor.RED}Error occurred during 'git commit' operation.{
+                    AnsiColor.RESET}"
+            )
         elif "git push" in command_name:
-            print_message(Messages.ERROR_TEXT,
-                          "Error occurred during 'git push' operation.")
+            print(
+                f"{AnsiColor.RED}Error occurred during 'git push' operation.{
+                    AnsiColor.RESET}"
+            )
 
         error_message = e.output.strip()
-        print_message(Messages.ERROR_TEXT, error_message)
-        print_message(Messages.FAILURE_CHANGES_NOT_PUSHED)
+        print(f"{AnsiColor.RED}{Messages.ERROR_TEXT}{
+              error_message}{AnsiColor.RESET}")
+        print(f"{AnsiColor.RED}{
+              Messages.FAILURE_CHANGES_NOT_PUSHED}{AnsiColor.RESET}")
 
         # Log the error and the failure
         date_and_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -131,32 +162,36 @@ def run_git_command(command: list[str], command_name: str, commit_message: str, 
 
 def main() -> None:
     """Main function to handle user input and run Git commands."""
+
     if not is_git_installed():
-        print_message(Messages.ERROR_GIT_NOT_INSTALLED)
+        print(f"{AnsiColor.RED}{
+              Messages.ERROR_GIT_NOT_INSTALLED}{AnsiColor.RESET}")
         date_and_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        logging.error(f"{date_and_time} | {
-                      Messages.ERROR_GIT_NOT_INSTALLED.text}")
+        logging.error(f"{date_and_time} | {Messages.ERROR_GIT_NOT_INSTALLED}")
         sys.exit(1)
 
+    default_branch_name = "master"
+
     try:
-        commit_message: str = set_commit_message()
+        commit_message: str = get_commit_message()
+        print(commit_message)
+        branch_name: str = get_branch_name(default_branch_name)
 
-        default_branch_name = "master"
-        branch_name: str = set_branch_name(default_branch_name)
-
-        print_message(Messages.INFORMATION_TEXT)
+        print(f"{AnsiColor.YELLOW}{Messages.INFORMATION_TEXT}{AnsiColor.RESET}")
 
         # Step 1: Add all of the files to the staging area
         git_add_command = ["git", "add", "."]
         run_git_command(git_add_command, "git add",
                         commit_message, branch_name)
-        print_message(Messages.SUCCESS_FILES_STAGED)
+        print(f"{AnsiColor.GREEN}{
+              Messages.SUCCESS_FILES_STAGED}{AnsiColor.RESET}")
 
         # Step 2: Commit the changes with the specified message
         git_commit_command = ["git", "commit", "-m", commit_message]
         run_git_command(git_commit_command, "git commit",
                         commit_message, branch_name)
-        print_message(Messages.SUCCESS_COMMIT_CREATED)
+        print(f"{AnsiColor.GREEN}{
+              Messages.SUCCESS_COMMIT_CREATED}{AnsiColor.RESET}")
 
         # Step 3: Push the changes to the remote repository
         git_push_command = ["git", "push", "-u", "origin", branch_name]
@@ -169,28 +204,32 @@ def main() -> None:
             commit_message} | Branch name: {branch_name} | Result: OK"
         logging.info(log_entry)
     except KeyboardInterrupt:
-        print_message(Messages.FAILURE_KEYBOARD_INTERRUPT)
-        print_message(Messages.FAILURE_CHANGES_NOT_PUSHED)
+        print(
+            f"{AnsiColor.RED}\n{Messages.FAILURE_KEYBOARD_INTERRUPT}{
+                AnsiColor.RESET}"
+        )
+        print(f"{AnsiColor.RED}{
+              Messages.FAILURE_CHANGES_NOT_PUSHED}{AnsiColor.RESET}")
 
         # Log the keyboard interrupt and failure
         try:
             commit_message = commit_message
-        except NameError:
+        except Exception:
             commit_message = "N/A"
 
         try:
             branch_name = branch_name
-        except NameError:
+        except Exception:
             branch_name = "N/A"
 
         date_and_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log_entry = f"{date_and_time} - Commit message: {commit_message} | Branch name: {
-            branch_name} | Result: FAIL | Error: {Messages.FAILURE_KEYBOARD_INTERRUPT.text}"
+            branch_name} | Result: FAIL | Error: {Messages.FAILURE_KEYBOARD_INTERRUPT}"
         logging.error(log_entry)
 
         sys.exit(1)
 
-    print_message(Messages.SUCCESS_CHANGES_PUSHED)
+    print(f"{AnsiColor.GREEN}{Messages.SUCCESS_CHANGES_PUSHED}{AnsiColor.RESET}")
 
 
 if __name__ == "__main__":
